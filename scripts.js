0d04b4d80e65f03f5e1c8baf290cd2eda86a9212
skappylmao <97861557+skappylmao@users.noreply.github.com>
2022-03-03 21:15:23 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+var myAwesomeVariable = "get out";
+
+for (var i = 0; i <10;i++) {
+    alert(myAwesomeVariable)
+}
+
+
+function Hello() {
+    document.write("Hi, Mom!")
+    document.write("<br>")
+    document.write("Hi, Dad!")
+}
